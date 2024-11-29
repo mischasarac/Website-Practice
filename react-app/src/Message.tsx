7706ed2522944423d758c547ced7fb282a6210f9
Moosh56 <mischa@COM7046>
2024-11-29 17:04:24 +1030

    Lets hope I didn't just delete my website

--- react-app/src/Message.tsx
+++
@@ -1,9 +0,0 @@
-// PascalCase: means capitalizing the first letter of each word in the name
-function Message() {
-  const name = "Daddy";
-  // JSX - JavaScript XML
-  // To see how JSX is converted to JS, go to https://babeljs.io/repl
-  return <h1>Hello {name}</h1>;
-}
-
-export default Message; // This is the default export of this module
